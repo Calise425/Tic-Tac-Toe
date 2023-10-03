c6d4adfa014003b6db18b2c5a1866f25e8b04a09
Calise425 <caitlin.norris1998@gmail.com>
2023-10-02 20:12:34 -0600

    Fixed the player display issue, fully functional as far as I'm aware

---
+++ src/index.js
@@ -0,0 +1,202 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+const TicTacToe = () => {
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [numPlayers, setNumPlayers] = useState(1);
+  const [playerDisplay, setPlayerDisplay] = useState("Player 1's Turn");
+  const [symbol, setSymbol] = useState("X");
+  const [gameOver, setGameOver] = useState(false);
+  const [winner, setWinner] = useState("");
+  const winningPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  // useEffect(() => {
+  //   if (gameOver) {
+  //     setPlayerDisplay(winner);
+  //   }
+  // }, [gameOver, winner]);
+
+  const checkWin = (boardArray) => {
+    for (let i = 0; i < winningPositions.length; i++) {
+      const [a, b, c] = winningPositions[i];
+      if (
+        boardArray[a] &&
+        boardArray[a] === boardArray[b] &&
+        boardArray[a] === boardArray[c]
+      ) {
+        setGameOver(true);
+        if (boardArray[a] === "X") {
+          if (numPlayers === 1) {
+            setWinner("You win!");
+          } else {
+            setWinner("Player 1 wins!");
+          }
+        } else if (boardArray[a] === "O") {
+          if (numPlayers === 1) {
+            setWinner("Computer wins");
+          } else {
+            setWinner("Player 2 wins!");
+          }
+        }
+      }
+    }
+    if (!boardArray.includes("")) {
+      setGameOver(true);
+      setWinner("It's a tie");
+      return;
+    }
+  };
+
+  const computerTurn = (tempArray) => {
+    if (gameOver) {
+      return;
+    }
+    for (let i = 0; i < winningPositions.length; i++) {
+      const [a, b, c] = winningPositions[i];
+      if (tempArray[a] && tempArray[a] === tempArray[b] && !tempArray[c]) {
+        tempArray[c] = "O";
+        setBoard(tempArray);
+        checkWin(tempArray);
+        setPlayerDisplay("Player 1's Turn");
+        return;
+      } else if (
+        tempArray[a] &&
+        tempArray[a] === tempArray[c] &&
+        !tempArray[b]
+      ) {
+        tempArray[b] = "O";
+        setBoard(tempArray);
+        checkWin(tempArray);
+        setPlayerDisplay("Player 1's Turn");
+        return;
+      } else if (
+        tempArray[b] &&
+        tempArray[b] === tempArray[c] &&
+        !tempArray[a]
+      ) {
+        tempArray[a] = "O";
+        setBoard(tempArray);
+        checkWin(tempArray);
+        setPlayerDisplay("Player 1's Turn");
+        return;
+      }
+    }
+    if (!tempArray[4]) {
+      tempArray[4] = "O";
+      setBoard(tempArray);
+      checkWin(tempArray);
+      setPlayerDisplay("Player 1's Turn");
+      return;
+    } else {
+      tempArray[tempArray.indexOf("")] = "O";
+      setBoard(tempArray);
+      checkWin(tempArray);
+      setPlayerDisplay("Player 1's Turn");
+      return;
+    }
+  };
+
+  const reset = () => {
+    setBoard(["", "", "", "", "", "", "", "", ""]);
+    setSymbol("X");
+    setPlayerDisplay("Player 1's Turn");
+    setWinner("");
+    setGameOver(false);
+  };
+
+  const cellClicked = (i) => {
+    if (gameOver) {
+      return;
+    } else {
+      const tempArray = [...board];
+      tempArray[i] = symbol;
+      setBoard(tempArray);
+      checkWin(tempArray);
+
+      if (!gameOver) {
+        if (numPlayers === 1) {
+          setPlayerDisplay("Computer's Turn");
+          setTimeout(() => {
+            computerTurn(tempArray);
+          }, 750);
+        } else {
+          symbol === "X" ? setSymbol("O") : setSymbol("X");
+          playerDisplay === "Player 1's Turn"
+            ? setPlayerDisplay("Player 2's Turn")
+            : setPlayerDisplay("Player 1's Turn");
+        }
+      }
+    }
+  };
+
+  return (
+    <div id="playing-field">
+      <h1>Tic-Tac-Toe</h1>
+      {!gameOver ? <h3>{playerDisplay}</h3> : null}
+      {gameOver ? <h3>{winner}</h3> : null}
+      <div id="board">
+        {board.map((cell, i) => {
+          if (!cell.length) {
+            return (
+              <div className="cell" key={i}>
+                <button
+                  className="cell-button"
+                  onClick={() => cellClicked(i)}
+                ></button>
+              </div>
+            );
+          } else if (cell === "X") {
+            return (
+              <div className="cell" key={i}>
+                <h2>X</h2>
+              </div>
+            );
+          } else {
+            return (
+              <div className="cell" key={i}>
+                <h2>O</h2>
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className="button-footer">
+        <button id="reset-button" onClick={(e) => reset()}>
+          Reset
+        </button>
+        {numPlayers === 1 ? (
+          <button
+            className="player-button"
+            onClick={() => {
+              setNumPlayers(2);
+              reset();
+            }}
+          >
+            One Player
+          </button>
+        ) : (
+          <button
+            className="player-button"
+            onClick={() => {
+              setNumPlayers(1);
+              reset();
+            }}
+          >
+            Two Players
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+ReactDOM.render(<TicTacToe />, document.getElementById("app"));
